8295460aa580857a153d67b63672e197211b93f8
ravalenziano <ravalenziano@gmail.com>
2021-03-14 15:17:51 -0400

    Tag helper for selecting tabs

---
+++ Conservice/Models/HtmlHelpers.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conservice.Models
+{
+    public static class MyHTMLHelpers
+    {
+        /// <summary>
+        /// https://stackoverflow.com/questions/20410623/how-to-add-active-class-to-html-actionlink-in-asp-net-mvc
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="controllers"></param>
+        /// <param name="actions"></param>
+        /// <param name="cssClass"></param>
+        /// <returns></returns>
+        public static string IsSelected(this IHtmlHelper htmlHelper, string controllers, string actions, string cssClass = "active")
+        {
+            string currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
+            string currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+
+            IEnumerable<string> acceptedActions = (actions ?? currentAction).Split(',');
+            IEnumerable<string> acceptedControllers = (controllers ?? currentController).Split(',');
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
+                cssClass : String.Empty;
+        }
+
+    }
+}
